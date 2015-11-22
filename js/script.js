31792159fd5f4e42b63e0983d756ad71ca296310
Zowrer <zourer@gmail.com>
2015-11-21 21:57:28 -0600

    limpiando archivos obsoletos

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
 jQuery(document).ready(function($){
-    $("#titulo").append("Bienvenido a mi plantilla");
 
-    //creatMenu(["Menu1", "Menu2", "Menu3"], ["#","#","#"]);
-
-    $("footer").append("@Zowrer");
 });
-
-function creatMenu(menus, linksMenus){
-    var n = menus.length;
-    for (var i = 1; i <= n; i++) {
-        var menu = $('#menu' + i);
-        if (i != n) {
-            var newMenu = menu.clone().attr('id', 'menu' + (i + 1));
-            menu.after(newMenu);
-        }
-        menu.children('a').attr('href', linksMenus[i-1]);
-        menu.children('a').html(menus[i-1]);
-    }
-}
