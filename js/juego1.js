c8e852df01ac234aba0e9c574b38792e5c374598
Zowrer <zourer@gmail.com>
2015-11-22 00:50:33 -0600

    elementos del primer juego, ya me voy a dormir XD

---
+++ js/juego1.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+    $.getJSON("dat/juego1.json", function(datos) {
+                $('#msj').html(datos[0].pregunta);
+                for (var i = 0; i < 5; i++) {
+                    var num = Math.floor((Math.random() * 20) + 1);
+                    if (datos[0].respuesta != num) {
+                        $('#options').append('<button class="option" id="op'+i+'">'+num+'</button>');
+                    }
+                }
+                $('#options').append('<button id="resp">'+datos[0].respuesta+'</button>');
+
+                $('.option').click(function(event) {
+                    alert("mal");
+                });
+
+                $('#resp').click(function(event) {
+                    alert("bien");
+                });
+            });
+});
