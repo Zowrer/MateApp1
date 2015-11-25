343f939b7f9c18e3890a42add92466ec1b84eb19
Zowrer <zourer@gmail.com>
2015-11-25 10:37:53 -0600

    arreglos al juego 1

--- js/juego1.js
+++ js/juego1.js
@@ -1,3 +1,9 @@
+var datos;
+var puntos;
+var limite;
+var ran;
+var pos;
+
 jQuery(document).ready(function($){
     $('#titulo').append('Fracciones en el Camino');
 
@@ -5,30 +11,47 @@ jQuery(document).ready(function($){
         src: '../img/grafica2.png'
     });
 
-    $.getJSON("dat/juego1.json", function(datos){
-        var limite = datos.length;
-        var ran = Math.floor(Math.random() * limite);
-        var pos = Math.floor(Math.random() * 5);
+    $.getJSON("dat/juego1.json", function(dat){
+        datos = dat;
+        limite = datos.length;
 
+        $.getJSON("dat/record.json", function(pts){
+            puntos = pts;
+            $_preguntar();
+        });
+    });
+
+
+    function $_preguntar(){
+        ran = Math.floor(Math.random() * limite);
+        pos = Math.floor(Math.random() * 5);
+
+        $('#puntos').html(puntos[0].puntos);
         $('#msj').html(datos[ran].pregunta);
 
         for (var i = 0; i < 5; i++) {
             var num = Math.floor((Math.random() * 20) + 1);
             if (datos[ran].respuesta != num) {
-                $('#options').append('<button class="option" id="op'+i+'">'+num+'</button>');
+                $('#options').append('<button class="option submit" id="op'+i+'">'+num+'</button>');
             }
 
             if (i == pos) {
-                $('#options').append('<button id="resp">'+datos[ran].respuesta+'</button>');
+                $('#options').append('<button class="submit" id="resp">'+datos[ran].respuesta+'</button>');
             }
         }
 
         $('.option').click(function(event) {
-            alert("mal");
+            $('#msjalert').html("Respuesta incorrecta");
+            $('.submit').detach();
+            $_preguntar();
         });
 
         $('#resp').click(function(event) {
-            alert("bien");
+            $('#msjalert').html("Muy bien");
+            puntos[0].puntos = parseInt(puntos[0].puntos) + 1;
+            $('.submit').detach();
+            $_preguntar();
         });
-    });
+    }
+
 });
