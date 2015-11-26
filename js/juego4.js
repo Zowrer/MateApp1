b2f56b1d6b7c98ab154a4e702bd4132f130b9e28
Zowrer <zourer@gmail.com>
2015-11-26 10:45:33 -0600

    juego 4

---
+++ js/juego4.js
@@ -0,0 +1,60 @@
+var datos;
+var puntos;
+var limite;
+var ran;
+var pos;
+
+
+jQuery(document).ready(function($){
+    $('#titulo').append('Fracciones');
+
+    $('#img').attr({
+        src: '../img/fracciones2.png'
+    });
+
+    $.getJSON("dat/juego4.json", function(dat){
+        datos = dat;
+        limite = datos.length;
+
+        puntos = JSON.parse(localStorage.prueba);
+
+        $_preguntar();
+    });
+
+
+    function $_preguntar(){
+        ran = Math.floor(Math.random() * limite);
+        pos = Math.floor(Math.random() * 5);
+
+        $('#puntos').html(puntos[3].puntos);
+        $('#msj').html(datos[ran].pregunta);
+
+        for (var i = 0; i < 5; i++) {
+            var num = Math.floor((Math.random() * 15) + 1);
+            if (datos[ran].respuesta != num) {
+                $('#options').append('<button class="option submit" id="op'+i+'">'+num+'</button>');
+            }
+
+            if (i == pos) {
+                $('#options').append('<button class="submit" id="resp">'+datos[ran].respuesta+'</button>');
+            }
+        }
+
+        $('.option').click(function(event) {
+            $('#msjalert').html("Respuesta incorrecta");
+            $('#imgresp').attr({ src: '../img/no.png' });
+            $('.submit').detach();
+            $_preguntar();
+        });
+
+        $('#resp').click(function(event) {
+            $('#msjalert').html("Muy bien");
+            $('#imgresp').attr({ src: '../img/ok.png' });
+            puntos[3].puntos = parseInt(puntos[3].puntos) + 1;
+            localStorage.setItem('prueba',JSON.stringify(puntos));
+            $('.submit').detach();
+            $_preguntar();
+        });
+    }
+
+});
